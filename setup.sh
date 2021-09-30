ad13f6150ffc9f5363f9fbf77dc5991d8e40d3d6
e-seng <eth.sengsavang@gmail.com>
2021-09-29 19:18:09 -0600

    Add setup.sh to repository
    hopefully will speed up the process to initialize a machine?

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+# this is here to set up a new machine? maybe?
+# either that or completely bork it lol
+
+# ~~~ setup ~~~
+# backup previous dot files in case i mess something up
+mkdir .backup
+mv .* .backup
+mv ~/dotfiles ~/.dotfiles
+
+# ~~~ .config ~~~
+# setup .config/
+mkdir ~/.config
+
+# link related files
+ln -s ~/.dotfiles/.config/nvim ~/.config/
+
+# ~~~ link base folders ~~~
+ln -s ~/.dotfiles/.dir_colors ~
+ln -s ~/.dotfiles/.zshrc ~
+ln -s ~/.dotfiles/.vimrc ~
+ln -s ~/bash
