3ca534ac0a3f277819b7f013f25df1989c5b1e5d
e-seng <eth.sengsavang@gmail.com>
2022-08-26 20:33:57 -0600

    Update polybar run script to ensure it runs with startx

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,8 +5,13 @@ killall -q polybar
 # If all your pars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
+# wait for all polybars to be killed
+while($(pgep polybar)); do
+  sleep 1;
+done;
+
 # Launch top bar
 echo "---" | tee -a /tmp/polybar_top.log
-polybar top 2>&1 | tee -a /tmp/polybar_top.log & disown
+polybar top 2>&1 | tee -a /tmp/polybar_top.log
 
 echo "Bars launched..."
