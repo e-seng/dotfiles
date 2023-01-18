00a78913c3860bc76a03a486f39886b4c67517d4
e-seng <eth.sengsavang@gmail.com>
2023-01-18 14:19:07 -0700

    Shutdown device if battery is too low
    this prevent the harddrive from dying (again whoops)

--- .local/bin/check-batt.sh
+++ .local/bin/check-batt.sh
@@ -15,18 +15,27 @@ BATTERY_DEVICE="BAT1";
 REALLY_LOW_BAT=10; # warn the user that the battery is *really* low
 LOW_BAT=15; # warn the user that the battery is low, a percentage
 
+SHUTDOWN_DELAY=10; # in minutes
+
 remaining_charge=`cat /sys/class/power_supply/$BATTERY_DEVICE/capacity`;
 
 cat /sys/class/power_supply/$BATTERY_DEVICE/status | grep "Discharging" > /dev/null
 battery_discharge=$?;
 
 if [[ $battery_discharge -ne 0 ]]; then
+  shutdown --show
+  if [[ $? -eq 0 ]]; then
+    echo "[`date`] Found future shutdown command, cancelling...";
+    shutdown -c --no-wall;
+  fi
+
   echo "[`date`] battery is charging";
   exit 0;
 fi;
 
 if [[ $remaining_charge -lt $REALLY_LOW_BAT ]]; then
-  notify-send -u critical "Battery Really Low!" "Charge your device immediately ($remaining_charge% remaining)";
+  notify-send -u critical "Battery Really Low!" "Charge your device immediately ($remaining_charge% remaining)\nShutting down in $SHUTDOWN_DELAY minutes";
+  shutdown -h $SHUTDOWN_DELAY --no-wall;
   exit 0;
 fi
 
