f35fdae2075894359533af3ca2c17ebb8defacf4
e-seng <eth.sengsavang@gmail.com>
2023-01-18 14:01:55 -0700

    Ensure check-batt is properly sending notifications

--- .local/bin/check-batt.sh
+++ .local/bin/check-batt.sh
@@ -7,11 +7,9 @@
 # XDG_RUNTIME_DIR=/run/user/1000
 # # change this to match the appropriate environment variable
 # DBUS_SESSION_BUS_ADDRESS='unix:path=/tmp/dbus-eCTdUoVf29,guid=4a4ee084da495313e7778a0e637db3ed'
+# # ^ this value is whatever is outputted by `echo $DBUS_SESSION_BUS_ADDRESS`
 # 
 # */1 * * * * /home/petiole/.local/bin/check-batt.sh > /dev/null
-
-SHELL=/bin/bash 
-PATH=/bin:/sbin:/usr/bin:/usr/sbin
 
 BATTERY_DEVICE="BAT1";
 REALLY_LOW_BAT=10; # warn the user that the battery is *really* low
