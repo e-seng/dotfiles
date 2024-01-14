de6ae38177c09b73fa93fc34545328e2747c8faa
e-seng <eth.sengsavang@gmail.com>
2024-01-14 18:53:51 -0500

    Update check-batt.sh to properly work with crontab

--- .local/bin/check-batt.sh
+++ .local/bin/check-batt.sh
@@ -6,10 +6,10 @@
 # XAUTHORITY=/home/petiole/.Xauthority
 # XDG_RUNTIME_DIR=/run/user/1000
 # # change this to match the appropriate environment variable
-# DBUS_SESSION_BUS_ADDRESS='unix:path=/tmp/dbus-eCTdUoVf29,guid=4a4ee084da495313e7778a0e637db3ed'
+# DBUS_SESSION_BUS_ADDRESS='<change with appropriate value>'
 # # ^ this value is whatever is outputted by `echo $DBUS_SESSION_BUS_ADDRESS`
 # 
-# */1 * * * * /home/petiole/.local/bin/check-batt.sh > /dev/null
+# */1 * * * * /home/petiole/.local/bin/check-batt.sh
 
 BATTERY_DEVICE="BAT1";
 REALLY_LOW_BAT=10; # warn the user that the battery is *really* low
